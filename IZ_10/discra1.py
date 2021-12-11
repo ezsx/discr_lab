60cc49b456fb3e3338438bd7bd09d26500a0fe6c
ezsx <scdcor@gmail.com>
2021-12-11 15:29:47 +0300

    Пересмотрена логика, обновлены функции, вывод программы совпадает с аналитическим решением.

--- IZ_10/discra1.py
+++ IZ_10/discra1.py
@@ -1,44 +1,153 @@
 from itertools import *
-
+import time
 d = ['a','b','c','d','e','f','g','h','j','k']
 #длина слова
 n = 7
 #кол-во повторений 3-ей буквы
 kn = 1
 
-# на входе строка ar , строка хх
-# получаем сочетания (не повторяющиеся !) элементов строки ar и хх
-def permut(ar,xx):
-    # строка очевидно тормозит, как перебороть, не понял
-    def concat():
-        return ''.join(ar[0:i])+xx[0]+''.join(ar[i:len(ar)])
+class Position:
 
-    # протаскиваем по одному символу xx, через ar пока все символы из xx (рекурсивно) не закончатся
-    if len(xx)>1:
-        x =xx[0]
-        for i in range(len(ar)):
-            if ar[i]==x:
-                continue
-            #рекурсивно вызываем, отрезая первый символ
-            yield from  permut(concat(),xx[1:])
-        # и надо еще  просто добавить в конец строки
-        yield from permut(''.join(ar)+xx[0],xx[1:])
-    else:
-        x =xx[0]
-        for i in range(len(ar)):
-            if ar[i]==x:
-                continue
-            yield concat()
-        yield ''.join(ar)+xx[0]
+    def __init__(self,pos_l,p_word=None):
+        # pos_l это ссылка на старшую (левую) позицию
+        if pos_l:
+            # ссылка на старшую (левую) позицию
+            self.pos_l = pos_l
+            # индекс позиции в этом слове (слева на право)
+            self.ind_x = pos_l.ind_x+1
+            # слово в котором представлена эта позиция
+            self.word:Word = pos_l.word
+        # Здесь когда pos_l = None, а слово задано.
+        # Это случай самой старшей позиции (самая левая)
+        else:
+            #(в слове крайняя левая, самая старшая, тут будет None)
+            self.pos_l = None
+            self.ind_x = 0
+            self.word:Word = p_word
+        # индекс текущего символа в данной позиции из словаря слова
+        self.cur_ind_c = self.word.get_next_char_idx(-1)
 
+    # выбрать следующий разрешенный на сейчас символ
+    def next(self,pos_r=None):
+        # если текуший символ из словаря не последний
+        if self.cur_ind_c < self.word.len_dict -1:
+            #возьмем следующий разрешенный символ из словаря в этом слове
+            idx = self.word.get_next_char_idx(self.cur_ind_c)
+        else:
+            # если последний, то с него надо уйти по любому
+            # чтобы освободить его для других позиций
+            self.word.cur_cnt[self.cur_ind_c] -=1
+            idx = -1
+
+        if idx < 0:
+            # Если след. символа нет, надо пойти с низу словаря
+            # но сначала спросим разрешния у левой позиции, если она есть
+            if self.pos_l:
+                # т.е. вызваем тот же самый метод, в котором находимся
+                # но для левой позиции, выглядит как рекурсия .
+                if self.pos_l.next(self):
+                    #левая позиция смогла переключится, и мы переключимся
+                    self.cur_ind_c = self.word.get_next_char_idx(-1)
+                    return self.cur_ind_c >= 0
+                else:
+                    #похоже приехали! не смогли найти свободный символ для этой позиции
+                    # или входные данные кривые или ошибка в логике.
+                    # Другой причины быть в этой части кода нет ((
+                    return False
+            else:
+                # здесь будем, когда уже потребовалось пойти с низу словаря (самый первый в списке - самый нижний)
+                # для самой левой (старшей) позиции, т.е. приехали, перебор закончен
+                self.word.stop()
+                return False
+        else:
+            self.cur_ind_c = idx
+            return True
+        return False
+
+
+
+
+class Word:
+
+    def __init__(self,p_dict:str,p_dict_cnt_in_wrd:dict,p_len_word=0):
+        # длина слов которые надо генерить на переданных данных
+        self.len_word = p_len_word
+        # словарь
+        self.dict_list = list(p_dict)
+        self.len_dict = len(self.dict_list)
+        # сколько раз символы в словаре должны  повторятся, по умолчанию 1
+        self.dict_cnt_list = [p_dict_cnt_in_wrd.get(c,1) for c in self.dict_list]
+        # текущее число повторов в слове для символов из словаря
+        self.cur_cnt = [0]*len(self.dict_list)
+        #Позиции list
+        self.pos_list = self.init_pos()
+        #самая крайняя правая (последня в слове позиция)
+        self.pos_last = self.pos_list[len(self.pos_list)-1]
+        self.is_has_next = True
+
+    def init_pos(self):
+
+        # надо нагенерить позиции таким образом
+        # чтобы получилось самое младшее слово из возможных
+        if self.len_word > 0:
+            #Нам задали длину, что делать еще не решил
+            pass
+        else:
+            # генерим макс возможное
+            s = 0
+            for i in self.dict_cnt_list:
+                s+=i
+            p  = [Position(None,self)]
+            for i in range(1,s):
+                p.append(Position(p[i-1]))
+            self.pos_last = p[len(p)-1]
+            return p
+
+
+
+    def next(self):
+        # переключится на след. слово
+        self.is_has_next = self.pos_last.next()
+
+
+    # выдает индекс след. символа разрешенного в слове после
+    # символа индекс которого p_ind_c
+    def get_next_char_idx(self,p_ind_c):
+        for idx_c in range(p_ind_c+1,self.len_dict):
+            if self.cur_cnt[idx_c] < self.dict_cnt_list[idx_c]:
+                # если сменить получилось
+                self.cur_cnt[idx_c]+=1
+                if p_ind_c >= 0:
+                    self.cur_cnt[p_ind_c]-=1
+                return idx_c
+        if p_ind_c < 0:
+            #что-то пошло не так, надо было перейти на новый символ
+            # но свободных нет ((
+            self.word.stop()
+        else:
+            # с текущего символа уходим по любому, так как
+            # вынуждены вернуть -1
+            self.cur_cnt[p_ind_c] -= 1
+        return -1
+
+    def stop(self):
+        self.is_has_next = False
+
+    def get_word(self):
+        return [self.dict_list[p.cur_ind_c] for p in self.pos_list]
+
+
+def permut_word(p_dict:str,p_dict_cnt_in_wrd:dict):
+    w = Word(p_dict,p_dict_cnt_in_wrd)
+    while w.is_has_next:
+        yield w.get_word()
+        w.next()
 
 def comb_c(ccc,f):
     g_cnt = 0
     # в полученно ccc элемент 1 и 2 повторяются 2 раза
     # 3 элемент повторяется kn раз
-    #мы берем три буквыб,которые встречаются обязательно в слове и рассчитываем сколько
-    #встретится таких же букв в слове еще
-    permut_str = ''.join(ccc[:2])+''.join([ccc[2]]*(kn-1))
+    permut_str = ''.join(ccc[:2]*2)+''.join([ccc[2]]*kn)
     #словарь без букв, которые сюда пришли и объязательны
     dd = list(set(d)-set(ccc))
     # строка перебора по оставшемуся словарю
@@ -48,18 +157,11 @@ def comb_c(ccc,f):
     #получаем комбинации
     for x in combinations(str_var,c):
         # теперь каждую комбинацию дополняем постоянной частью что пришла сюда
-        str_var_c = ''.join(x)
-        str_cv = ''.join(ccc) + str_var_c
-        # так как потом будем дополнять еще 2 буквы по разу и kn без одной
-        #nc = n-2-kn+1
-        nc=len(str_cv)
-        #получаем все возможные сочетания из текущей комбинации
-        for y in permutations(str_cv, nc):
-            # домнажаем на массив входных букв
-            for y1 in permut(y,tuple(permut_str)):
-                f.write(''.join(y1)+'\n')
-                g_cnt = g_cnt +1
-                # print(''.join(y1))
+        str_var_c = ''.join(ccc)+''.join(x)
+        for y in permut_word(str_var_c,{ccc[0]:2,ccc[1]:2,ccc[2]:kn}):
+            f.write(''.join(y)+'\n')
+            g_cnt = g_cnt +1
+            # print(''.join(y1))
     return g_cnt
 
 
@@ -80,6 +182,7 @@ def m_c(f):
 
 
 def main():
+    time_start=time.perf_counter()
     if kn>n-4:
         raise Exception("кол-во повторений буквы  не может быть больше %d"%(n-4))
     if kn<1:
@@ -89,9 +192,13 @@ def main():
     f =  open('workfile','w')
     m_c(f)
     f.close()
+    time_end=time.perf_counter()
+    print(time_end-time_start)
 
-# main()
+main()
 
-
-for x in permut('asdf','q1'):
-    print(x)
+# cnt = 0
+# for x in permut_word('12345',{'1':2,'2':2}):
+#     print(x)
+#     cnt+=1
+# print (cnt)
