8ca18c925f2ff284b4087a2458f2c7af18d72773
ezsx <scdcor@gmail.com>
2021-12-19 15:19:50 +0300

    посчитал

---
+++ IZ_9/discr9.py
@@ -0,0 +1,18 @@
+s="_"
+
+def S(k,n,m):
+    m+=1
+    print("(",m,s*m,")","S(%d,%d)=" % (k, n),"S(%d, %d)+%d*S(%d, %d)" % (k - 1, n - 1,k, k, n - 1))
+    if k > n:
+        print("S(%d,%d)=0" % (k, n))
+        return 0
+    if k == n:
+        print("S(%d,%d)=1" % (k, n))
+        return 1
+    if k == 1:
+        print("S(%d,%d)=1" % (k, n))
+        return 1
+
+    return S(k-1, n-1,m)+k*S(k, n-1,m)
+m = 0
+print(S(5,10,m))
