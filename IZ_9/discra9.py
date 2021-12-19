84c97d4a49da335244129d2d2f142db6a1e70064
ezsx <scdcor@gmail.com>
2021-12-20 01:34:13 +0300

    оформил 9.2.3

--- IZ_9/discra9.py
+++ IZ_9/discra9.py
@@ -1,14 +1,18 @@
 from math import *
-
-def sum(p,k):
+ss="_"
+def sum(p,k,m):
     s=0
     for i in range(1,p):
-        s+=comb(p,i)*pow(2,(i*(p-i)))*C(i,k-1)
+        s+=comb(p,i)*pow(2,(i*(p-i)))*C(i,k-1,m)
     return s
-def C(p,k):
+def C(p,k,m):
+    m += 1
     if k==1:
         return 1
     if k>p:
         return 0
-    return (1/k)*sum(p,k)
-print(C(8,8))
+
+    print("(",m,ss*m,")","C(%d,%d)=%d" % (p, k, (1 / k) * sum(p, k,m)))
+    return (1/k)*sum(p,k,m)
+
+print(C(8,8,0))
