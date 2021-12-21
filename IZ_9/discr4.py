fef6f1e9f2fa6e0b728c7cece0cc44c0ce6cfe40
ezsx <scdcor@gmail.com>
2021-12-21 03:21:26 +0300

    9.1.5. Построить все подстановки на множестве цифр, содержащих 5 независимых цикла.

---
+++ IZ_9/discr4.py
@@ -0,0 +1,14 @@
+# 9.1.4. Дано множество A = { a, a, a, a, a, a, a, a, a, a}. Сколько существует способов
+# разбить его на 5 именованных множеств?
+from math import *
+def count(n,k):
+    kof=int(n/k)
+    s=0
+    s1=0
+    for i in range(1,n+1):
+        s+=1
+        if s%kof==0:
+            s1+=1
+    return comb(s+s1-1,kof)
+
+print(count(10,5))
